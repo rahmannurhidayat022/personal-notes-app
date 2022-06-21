31dce835f259d671cda53a14c538d1fc1f3b2402
rahmannurhidayat022 <rahmannurhidayat022@gmail.com>
2022-06-21 14:12:53 +0700

    created brand logo

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
+import Navbar from '../components/Navbar'
 
 export default class Notes extends Component {
   render() {
     return (
-      <div>Hello World</div>
+      <>
+        <Navbar/>
+      </>
     )
   }
 }
